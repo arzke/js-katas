24c9807b1ea7accdf5b23a226c88d25d7c176f19
Bruno Arsene <arsene.bruno@gmail.com>
2016-06-29 19:37:32 +0200

    Create treeData
    treeData contains a tree as a literal object.

---
+++ tree/src/tree/treeData.js
@@ -0,0 +1,40 @@
+const treeData = [
+  {
+    id: 1,
+    children: []
+  },
+  {
+    id: 2,
+    children: [
+      {
+        id: 3,
+        children: []
+      },
+      {
+        id: 4,
+        children: [
+          {
+            id: 5,
+            children: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 6,
+    children: [
+      {
+        id: 7,
+        children: []
+      },
+      {
+        id: 8,
+        children: []
+      }
+    ]
+  }
+];
+
+
+export default treeData;
