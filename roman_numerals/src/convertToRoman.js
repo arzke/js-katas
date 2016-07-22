a1796b4bd8892754df8e4294982f08a852059b4f
Bruno Arsene <arsene.bruno@gmail.com>
2016-07-22 02:26:27 +0200

    Refactor

--- roman_numerals/src/convertToRoman.js
+++ roman_numerals/src/convertToRoman.js
@@ -9,22 +9,17 @@ const numbers = {
 };
 
 function convertToRoman(number) {
-    if(number < 1 && number % 10 === 0) {
+    if(number === 0) {
         return '';
     }
 
-    let romanNumeral = getNumber(number, numbers);
-
-    if(romanNumeral.charAt(0) === 'V') {
-        return romanNumeral + convertToRoman(number - 5);
-    }
-
-    return romanNumeral.repeat(numbers[number] ? 1 : number.toString().charAt(0)) + convertToRoman(+number.toString().substr(1));
+    let closestNumber = getNumber(number, numbers);
+    return numbers[closestNumber] + convertToRoman(number - closestNumber);
 }
 
 function getNumber(number, numbers) {
     let smallerNumbers = Object.keys(numbers).filter(n => n <= number);
-    return numbers[Math.max.apply(null, smallerNumbers)];
+    return Math.max.apply(null, smallerNumbers);
 }
 
 export default convertToRoman;
